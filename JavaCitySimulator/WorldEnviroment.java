b8770fe1dbf6d4b1aa0044cabc4eed323451fd4f
Timecube-6624 <2515517483@qq.com>
2026-02-14 14:42:52 +0800

    New alpha version 2.0.1
    We delete our old code but start a new alpha version to solove bugs
    In this version,we try to use *log.txt* to save logs instead of using terminal

---
+++ JavaCitySimulator/WorldEnviroment.java
@@ -0,0 +1,10 @@
+
+import java.io.File;
+import java.io.IOException;
+
+public class WorldEnviroment {
+	public static void main(String[] args) {
+        CreateLogFile log = new CreateLogFile();
+        log.newLog();
+    }
+}
